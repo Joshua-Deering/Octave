365a3e2059d3aa8f7e6671ef0f544911eb54eada
Joshua-Deering <jidjosh2006@icloud.com>
2025-01-21 17:09:12 -0500

    Refactoring - Removed unused functions - Commented currently unused functions that may be used in future

--- src/players.rs
+++ src/players.rs
@@ -2,45 +2,43 @@ use cpal::Data;
 use std::fs::File;
 use std::io::{BufReader, Seek, SeekFrom};
 
-use crate::audio::{do_fourier_transform, do_short_time_fourier_transform};
-use crate::audio::{FreqData, ShortTimeDftData, WindowFunction};
-use crate::file_io::WavInfo;
-use crate::{read_data_interleaved_unchecked, read_wav_meta};
+use crate::file_io::{WavInfo, read_data_interleaved_unchecked};
+use crate::audio::{WindowFunction, ShortTimeDftData, do_short_time_fourier_transform};
+use crate::read_wav_meta;
 
-#[allow(unused)]
 pub struct SignalPlayer {
     pub samples: Vec<Vec<f32>>,
     pub sample_rate: u32,
     channels: usize,
-    duration: f32,
-    pos: usize,
+    //duration: f32,
+    //pos: usize,
 }
 
-#[allow(unused)]
 impl SignalPlayer {
     pub fn new(samples: Vec<Vec<f32>>, sample_rate: u32, channels: usize) -> Self {
-        let duration = samples.len() as f32 / sample_rate as f32;
+        //let duration = samples.len() as f32 / sample_rate as f32;
         Self {
             samples,
             sample_rate,
             channels,
-            duration,
-            pos: 0,
+            //duration,
+            //pos: 0,
         }
     }
-    pub fn next_chunk(&mut self, data: &mut Data) {
-        let dat_slice = data.as_slice_mut().unwrap();
-        let end = self.pos + (dat_slice.len() / self.channels);
-        if end >= self.samples[0].len() {
-            return;
-        }
-        for i in self.pos..end {
-            for c in 0..self.channels {
-                dat_slice[(i - self.pos) * self.channels + c] = self.samples[c][i];
-            }
-        }
-        self.pos += data.len() / self.channels;
-    }
+
+    //pub fn next_chunk(&mut self, data: &mut Data) {
+    //    let dat_slice = data.as_slice_mut().unwrap();
+    //    let end = self.pos + (dat_slice.len() / self.channels);
+    //    if end >= self.samples[0].len() {
+    //        return;
+    //    }
+    //    for i in self.pos..end {
+    //        for c in 0..self.channels {
+    //            dat_slice[(i - self.pos) * self.channels + c] = self.samples[c][i];
+    //        }
+    //    }
+    //    self.pos += data.len() / self.channels;
+    //}
 
     pub fn do_short_time_fourier_transform(
         &self,
@@ -70,21 +68,6 @@ impl SignalPlayer {
             freqs,
             self.sample_rate,
         )
-    }
-
-    pub fn do_fourier_transform(&self) -> Vec<Vec<FreqData>> {
-        let mut out = vec![vec![]; self.channels];
-        for i in 0..self.channels {
-            out[i] = do_fourier_transform(&self.samples[i], self.sample_rate);
-        }
-        out
-    }
-
-    pub fn do_fourier_transform_on_channel(&self, channel: usize) -> Vec<FreqData> {
-        if channel > self.channels {
-            return vec![];
-        }
-        do_fourier_transform(&self.samples[channel - 1], self.sample_rate)
     }
 }
 
