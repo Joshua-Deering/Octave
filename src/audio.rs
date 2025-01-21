bdc67cfd8a60e144dec66b9cd540fa6a3bdc1179
Joshua-Deering <jidjosh2006@icloud.com>
2025-01-21 17:07:09 -0500

    Added new DFT system - Moved DFT logic to fft.rs - fft.rs now can use any dft that implements the DFT trait - dfts now precompute window values and twiddle factors - removed old dft implementation from audio.rs - removed unused functions from audio.rs - implemented actual Hann window instead of bell curve

--- src/audio.rs
+++ src/audio.rs
@@ -1,15 +1,9 @@
-use std::{f32::consts::{PI, TAU}, fmt, mem::size_of_val, ops::{AddAssign, Div, DivAssign, Mul}, thread};
+use std::{f32::consts::{PI, TAU}, fmt, mem::size_of_val};
 use fastapprox::fast;
 
-#[allow(unused)]
-pub fn generate_multichannel_signal(freq_data: &Vec<Vec<FreqData>>, sample_rate: usize, duration: f32) -> Vec<Vec<f32>> {
-    let mut output = vec![vec![]; freq_data.len()];
-    for i in 0..freq_data.len() {
-        output[i] = generate_signal(&freq_data[i], sample_rate, duration);
-    }
-    output
-}
+use crate::fft::{create_dft, DftType};
 
+#[allow(unused)]
 pub fn generate_signal(freq_data: &Vec<FreqData>, sample_rate: usize, duration: f32) -> Vec<f32> {
     let mut samples = vec![0.; (sample_rate as f32 * duration) as usize];
     for i in 0..samples.len() {
@@ -55,12 +49,14 @@ pub fn do_short_time_fourier_transform(samples: &Vec<f32>, sample_rate: u32, win
     let overlap_size = (samples_per_window as f32 * overlap).round() as usize;
     let step_size = samples_per_window - overlap_size;
     let num_windows = samples.len() / (samples_per_window - overlap_size);
+
+    let dft = create_dft(Some(DftType::NAIVE), sample_rate, samples_per_window, window_func);
     
     let mut out: Vec<Vec<FreqData>> = vec![vec![]; num_windows];
     let mut window_idx = 0;
     let mut i: usize = 0;
     while i + samples_per_window < samples.len() {
-        out[window_idx] = do_fourier_transform_slice(&samples[i..i+samples_per_window], sample_rate, window_func);
+        out[window_idx] = dft.process(&samples[i..i+samples_per_window]);
         window_idx += 1;
         i += step_size;
     }
@@ -74,91 +70,17 @@ pub fn do_short_time_fourier_transform(samples: &Vec<f32>, sample_rate: u32, win
     out
 }
 
-
-pub fn do_fourier_transform_slice(samples: &[f32], sample_rate: u32, window_func: WindowFunction) -> Vec<FreqData> {
-    let num_frequencies = samples.len() / 2 + 1;
-    let mut frequency_data = vec![FreqData::ZERO; num_frequencies];
-    let frequency_step = sample_rate as f32 / samples.len() as f32;
-    let chunk_size = num_frequencies/6;
-
-    let window_bound_adj = f32::powi(4. / samples.len() as f32, 2);
-    let window = | idx: usize | {
-        if window_func == WindowFunction::Square {
-            1.
-        } else {
-            1./((idx * idx) as f32 * window_bound_adj + 1.)
-        }
-    };
-    
-    thread::scope(|s| {
-        for (idx, frequencies) in frequency_data.chunks_mut(chunk_size).enumerate() {
-            s.spawn(move || {
-                for freq_index in idx*chunk_size..idx*chunk_size+frequencies.len() {
-                    let mut sum = Vec2::ZERO;
-                    for i in 0..samples.len() {
-                        let mut angle = (i as f32/samples.len() as f32) * TAU * freq_index as f32;
-                        angle = ((angle + PI) % TAU) - PI;
-                        let test_pt = Vec2::new(fast::cos(angle), fast::sin(angle));
-                        sum += test_pt * samples[i] * window(i);
-                    }
-                    let is_nyquist_or_zero = (freq_index == num_frequencies-1 && samples.len() % 2 == 0) || freq_index == 0;
-
-                    let sample_center = sum / samples.len();
-                    let ampl = if is_nyquist_or_zero {sample_center.magnitude()} else {sample_center.magnitude() * 2.};
-                    frequencies[freq_index - idx*chunk_size].frequency = (freq_index as f32 * frequency_step) as f32;
-                    frequencies[freq_index - idx*chunk_size].amplitude = ampl;
-                    frequencies[freq_index - idx*chunk_size].phase = -f32::atan2(sample_center.y, sample_center.x);
-                }
-            });
-        }
-    });
-
-    frequency_data
-}
-
-pub fn do_fourier_transform(samples: &Vec<f32>, sample_rate: u32) -> Vec<FreqData> {
-    let num_frequencies = samples.len() / 2 + 1;
-    let mut frequency_data = vec![FreqData::ZERO; num_frequencies];
-    let frequency_step = sample_rate as f32 / samples.len() as f32;
-    let chunk_size = num_frequencies/6;
-    
-    thread::scope(|s| {
-        for (idx, frequencies) in frequency_data.chunks_mut(chunk_size).enumerate() {
-            s.spawn(move || {
-                for freq_index in idx*chunk_size..idx*chunk_size+frequencies.len() {
-                    let mut sum = Vec2::ZERO;
-                    for i in 0..samples.len() {
-                        let mut angle = (i as f32/samples.len() as f32) * TAU * freq_index as f32;
-                        angle = ((angle + PI) % TAU) - PI;
-                        let test_pt = Vec2::new(fast::cos(angle), fast::sin(angle));
-                        sum += test_pt * samples[i];
-                    }
-                    let is_nyquist_or_zero = (freq_index == num_frequencies-1 && samples.len() % 2 == 0) || freq_index == 0;
-
-                    let sample_center = sum / samples.len();
-                    let ampl = if is_nyquist_or_zero {sample_center.magnitude()} else {sample_center.magnitude() * 2.};
-                    frequencies[freq_index - idx*chunk_size].frequency = (freq_index as f32 * frequency_step) as f32;
-                    frequencies[freq_index - idx*chunk_size].amplitude = ampl;
-                    frequencies[freq_index - idx*chunk_size].phase = -f32::atan2(sample_center.y, sample_center.x);
-                }
-            });
-        }
-    });
-
-    frequency_data
-}
-
 #[derive(Eq, PartialEq, Clone, Copy)]
 pub enum WindowFunction {
     Square,
-    BellCurve,
+    Hann,
 }
 
 impl WindowFunction {
     pub fn from_str(str: &str) -> Option<Self> {
         match str.to_lowercase().as_str() {
             "square" => Some(WindowFunction::Square),
-            "bellcurve" => Some(WindowFunction::BellCurve),
+            "hann" => Some(WindowFunction::Hann),
             _ => None
         }
     }
@@ -166,7 +88,7 @@ impl WindowFunction {
     pub fn to_string(&self) -> String {
         match self {
             Self::Square => "Square".into(),
-            Self::BellCurve => "BellCurve".into()
+            Self::Hann => "Hann".into()
         }
     }
 }
@@ -175,7 +97,7 @@ impl From<usize> for WindowFunction {
     fn from(value: usize) -> Self {
         match value {
             0 => Self::Square,
-            1 => Self::BellCurve,
+            1 => Self::Hann,
             _ => Self::Square
         }
     }
@@ -190,47 +112,4 @@ pub struct FreqData {
 
 impl FreqData {
     pub const ZERO: Self = FreqData { frequency: 0., amplitude: 0., phase: 0. };
-}
-
-struct Vec2 {
-    pub x: f32,
-    pub y: f32
-}
-
-impl Vec2 {
-    const ZERO: Self = Vec2 { x: 0., y: 0. };
-    fn new(x: f32, y: f32) -> Self {
-        Self { x, y }
-    }
-    fn magnitude(&self) -> f32 {
-        f32::sqrt(self.x * self.x + self.y * self.y)
-    }
-}
-
-impl AddAssign for Vec2 {
-    fn add_assign(&mut self, rhs: Self) {
-        self.x += rhs.x;
-        self.y += rhs.y;
-    }
-}
-
-impl Mul<f32> for Vec2 {
-    type Output = Self;
-    fn mul(self, rhs: f32) -> Self::Output {
-        return Self { x: self.x * rhs, y: self.y * rhs }
-    }
-}
-
-impl Div<usize> for Vec2 {
-    type Output = Self;
-    fn div(self, rhs: usize) -> Self::Output {
-        return Self { x: self.x / rhs as f32, y: self.y / rhs as f32}
-    }
-}
-
-impl DivAssign<f32> for Vec2 {
-    fn div_assign(&mut self, rhs: f32) {
-        self.x /= rhs;
-        self.y /= rhs;
-    }
 }
--- src/fft.rs
+++ src/fft.rs
@@ -1,10 +1,156 @@
+use std::{thread, num::NonZero, f32::consts::{PI, TAU}, ops::{AddAssign, Div, Mul}};
+use crate::audio::{FreqData, WindowFunction};
+
+pub fn create_dft(dft_type: Option<DftType>, sample_rate: u32, buffer_size: usize, window_function: WindowFunction) -> Box<dyn DFT> {
+    // if dft_type is specified, just return that
+    if let Some(tp) = dft_type {
+        match tp {
+            DftType::NAIVE => return Box::new(NaiveDft::new(sample_rate, buffer_size, window_function)),
+        }
+    }
+    //otherwise, do some stuff to figure out which dft to use
+    Box::new(NaiveDft::new(sample_rate, buffer_size, window_function))
+}
 
 pub trait DFT {
-    fn process(&self);
+    // should panic if given the wrong size buffer (buffer.len != self.buffer_size)
+    fn process(&self, buffer: &[f32]) -> Vec<FreqData>;
+}
+
+pub enum DftType {
+    NAIVE
+}
+
+//pub struct Fft {
+//
+//}
+
+pub struct NaiveDft {
+    pub frequency_step: f32,
+    pub buffer_size: usize,
+    num_frequencies: usize,
+    twiddle_factors: Vec<Complex>,
+    window_function: Vec<f32>,
+    available_threads: NonZero<usize>
+}
+
+impl NaiveDft {
+    pub fn new(sample_rate: u32, buffer_size: usize, window_function: WindowFunction) -> Self {
+        let frequency_step = sample_rate as f32 / buffer_size as f32;
+        let num_frequencies = buffer_size / 2 + 1;
+        let mut twiddle_factors: Vec<Complex> = Vec::with_capacity(num_frequencies * buffer_size);
+
+        for f in 0..num_frequencies {
+            for i in 0..buffer_size {
+                let mut angle = (i as f32/buffer_size as f32) * TAU * f as f32;
+                angle = ((angle + PI) % TAU) - PI;
+                twiddle_factors.push(Complex::new(f32::cos(angle), f32::sin(angle)));
+            }
+        }
+        
+        let available_threads = thread::available_parallelism().unwrap();
+        
+        // pre-compute the values for the window function
+        let mut window_function_vec: Vec<f32> = Vec::with_capacity(buffer_size);
+        match window_function {
+            WindowFunction::Hann => {
+                for i in 0..buffer_size {
+                    let cur_hann = f32::powi(f32::sin((PI * i as f32) / buffer_size as f32), 2);
+                    window_function_vec.push(cur_hann);
+                }
+            },
+            WindowFunction::Square => {
+                window_function_vec.fill(1f32);
+            }
+        }
+
+        Self {
+            frequency_step,
+            buffer_size,
+            num_frequencies,
+            twiddle_factors,
+            window_function: window_function_vec,
+            available_threads,
+        }
+    }
+}
+
+impl DFT for NaiveDft {
+    fn process(&self, buffer: &[f32]) -> Vec<FreqData> {
+        assert!(buffer.len() == self.buffer_size);
+        
+        let mut frequencies = vec![FreqData::ZERO; self.num_frequencies];
+        let chunk_size = self.num_frequencies / self.available_threads;
+        
+        thread::scope(|s| {
+            for (idx, frequency_block) in frequencies.chunks_mut(chunk_size).enumerate() {
+                s.spawn(move || {
+                    let chk_pos = idx * chunk_size;
+                    for f in chk_pos..chk_pos + frequency_block.len() {
+                        let mut sum = Complex::zero();
+                        for i in 0..self.buffer_size {
+                            sum += &self.twiddle_factors[f * self.buffer_size + i] * (buffer[i] * self.window_function[i]);
+                        }
+
+                        let sample_center = sum / self.buffer_size as f32;
+                        frequency_block[f - chk_pos].frequency = (f as f32 * self.frequency_step) as f32;
+                        frequency_block[f - chk_pos].amplitude = sample_center.magnitude() * 2.;
+                        frequency_block[f - chk_pos].phase = -f32::atan2(sample_center.i, sample_center.r);
+                    }
+                });
+            }
+        });
+
+        frequencies[0].amplitude /= 2.;
+        frequencies[self.num_frequencies-1].amplitude /= 2.;
+        
+        frequencies
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Complex {
-    pub r: f64,
-    pub i: f64,
+    pub r: f32,
+    pub i: f32,
+}
+
+impl Complex {
+    fn zero() -> Self {
+        Self { r: 0., i: 0. }
+    }
+    
+    fn new(r: f32, i: f32) -> Self {
+        Self { r, i }
+    }
+
+    fn magnitude(&self) -> f32 {
+        f32::sqrt(self.r * self.r + self.i * self.i)
+    }
+}
+
+impl AddAssign for Complex {
+    fn add_assign(&mut self, rhs: Self) {
+        self.r += rhs.r;
+        self.i += rhs.i;
+    }
+}
+
+impl Div<f32> for Complex {
+    type Output = Complex;
+    fn div(self, rhs: f32) -> Self::Output {
+        Complex {
+            r: self.r / rhs,
+            i: self.i / rhs
+        }
+    }
+}
+
+impl<'a> Mul<f32> for &'a Complex {
+    type Output = Complex;
+    fn mul(self, rhs: f32) -> Self::Output {
+        Complex {
+            r: self.r * rhs,
+            i: self.i * rhs,
+        }
+    }
 }
